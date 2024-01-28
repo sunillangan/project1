09c78cec7cc5ad3010822bc37fbbf736236edd68
sunillangan <58847457+sunillangan@users.noreply.github.com>
2024-01-29 00:25:40 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,42 @@
-resource "aws_instance" "myec2" {
-  ami           = "ami-0a3c3a20c09d6f377" # us-west-2
-  instance_type = "t2.micro"
-}
+provider "aws" {
+	  region  = "us-east-1"
+	}
+	
+	
+	resource "aws_security_group" "allow_tls" {
+	  name = "shah-security_group"
+	  description = "Allow TLS inbound traffic"
+	
+	  ingress {
+	    from_port = 443
+	    to_port = 443
+	    protocol = "tcp"
+	    cidr_blocks = var.vpn_ip
+	  }
+	
+	  ingress {
+	    from_port = 80
+	    to_port = 80
+	    protocol = "tcp"
+	    cidr_blocks = var.vpn_ip
+	  }
+	
+	  ingress {
+	    from_port = 8080
+	    to_port = 8080
+	    protocol = "tcp"
+	    cidr_blocks = var.vpn_ip
+	  }
+	
+	  egress {
+	    from_port        = 0
+	    to_port          = 0
+	    protocol         = "-1"
+	    cidr_blocks      = ["0.0.0.0/0"]
+	    ipv6_cidr_blocks = ["::/0"]
+	  }
+	
+	  tags = {
+	    Name = "allow_tls"
+	  }
+	}
